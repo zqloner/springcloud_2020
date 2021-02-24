8a12d2cb2e74d46e31a29678d8cc31fe8b892c30
zqloner <zqloner@163.com>
2021-02-24 20:12:17 +0800

    ...

---
+++ cloud-eureka-server7003/src/main/java/com/zq/springcloud/EurekaMain7003.java
@@ -0,0 +1,19 @@
+package com.zq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/1/2911:38
+ * @Company: MGL
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7003.class, args);
+    }
+}
