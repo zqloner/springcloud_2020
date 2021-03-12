2858e9a96b381b116e367c4b54518e327f0e2034
zqloner <zqloner@163.com>
2021-03-12 14:46:44 +0800

    ...多服务提供服务

---
+++ cloud-provider-payment8002/src/main/java/com/zq/springcloud/PaymentMain8002.java
@@ -0,0 +1,19 @@
+package com.zq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/1/2716:04
+ * @Company: MGL
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class, args);
+    }
+}
