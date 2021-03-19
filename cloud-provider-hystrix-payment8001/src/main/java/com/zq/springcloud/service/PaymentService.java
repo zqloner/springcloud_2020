50187c3f2d17536af2aa79a7f5fbcd5f855f67ed
zqloner <zqloner@163.com>
2021-03-19 16:56:55 +0800

    Hystrix服务端单个方法加@HystrixCommond添加fallback降级处理方法

--- cloud-provider-hystrix-payment8001/src/main/java/com/zq/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zq/springcloud/service/PaymentService.java
@@ -1,8 +1,8 @@
 package com.zq.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @Description:
@@ -18,9 +18,20 @@ public class PaymentService {
     }
 
     //失败
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name ="execution.isolation.thread.timeoutInMilliseconds",value = "3000")  //3秒钟以内就是正常的业务逻辑
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 3;
-        try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
-        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+//        int timeNumber = 5; //休眠五秒
+          int age = 10 / 0;  //创造异常
+
+//        try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
+
+//        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"O(∩_∩)O哈哈~"+" 耗时(秒)"+timeNumber;
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"O(∩_∩)O哈哈~"+" 耗时(秒)";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"   当前系统繁忙,请您稍后再试,id：  "+id+"\t"+"o(╥﹏╥)o";
     }
 }
