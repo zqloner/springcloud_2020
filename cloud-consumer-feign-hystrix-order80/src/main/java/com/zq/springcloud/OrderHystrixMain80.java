09e6c2f1ab2bbd6ec37d51ed7b76e7c71f052075
zqloner <zqloner@163.com>
2021-03-19 16:27:10 +0800

    演示某个服务繁忙,造成其他服务出现等待案例

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/zq/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,19 @@
+package com.zq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/3/1915:19
+ * @Company: MGL
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/zq/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,41 @@
+package com.zq.springcloud.controller;
+
+import com.zq.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/3/1915:23
+ * @Company: MGL
+ */
+@RestController
+@Slf4j
+public class OrderHystrixController {
+
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        log.info("*******result:"+result);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        log.info("*******result:"+result);
+        return result;
+    }
+}
