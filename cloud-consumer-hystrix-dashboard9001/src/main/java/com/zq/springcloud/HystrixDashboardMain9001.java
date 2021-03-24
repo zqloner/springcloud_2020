d8363b3975a57903787aecdcbb21d994e85651b8
zqloner <zqloner@163.com>
2021-03-24 17:14:23 +0800

    --Dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/zq/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.zq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/3/2415:59
+ * @Company: MGL
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
