546cbb323cc7d21a51911f18da8e89d1f0b744cd
zqloner <zqloner@163.com>
2021-03-24 20:22:06 +0800

    路由第一种方式。配置文件方式

---
+++ cloud-gateway-gateway9527/src/main/java/com/zq/springcloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.zq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/3/24--->19:52
+ * @Company: MGL
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run( GateWayMain9527.class,args);
+    }
+}
