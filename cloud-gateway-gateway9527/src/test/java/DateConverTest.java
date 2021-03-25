0b60f5eaacbe82ab896b411c73a8ab7583ed7197
zqloner <zqloner@163.com>
2021-03-25 20:52:56 +0800

    Filter相关配置和操作

---
+++ cloud-gateway-gateway9527/src/test/java/DateConverTest.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/3/24--->21:31
+ * @Company: MGL
+ */
+public class DateConverTest {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
