e5f572d1ff8327a94a6753f103ec284fd0f2c574
zqloner <zqloner@163.com>
2021-03-25 23:32:57 +0800

    Stream的分组解决。消息重复消费和持久化问题

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/zq/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,27 @@
+package com.zq.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/3/25--->22:45
+ * @Company: MGL
+ */
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者2号，接受："+message.getPayload()+"\t port:"+serverPort);
+    }
+}
