b1975a12f8886fefcd5a9048a9d2e5ecdbbe6e9a
zqloner <zqloner@163.com>
2021-03-25 13:35:30 +0800

    --Config-client

---
+++ cloud-config-client-3355/src/main/java/com/zq/springcloud/contoller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.zq.springcloud.contoller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/3/2510:33
+ * @Company: MGL
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
