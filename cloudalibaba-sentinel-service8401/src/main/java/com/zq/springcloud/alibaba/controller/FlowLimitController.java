25e7b24f5b2ed0656d93ceb59b02cff8c8775537
zqloner <zqloner@163.com>
2021-03-30 13:29:09 +0800

    -hotKey--->methods。

--- cloudalibaba-sentinel-service8401/src/main/java/com/zq/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zq/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.zq.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -50,5 +53,18 @@ public class FlowLimitController{
         log.info("testE 异常数");
         int age = 10/0;
         return "testE------测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    //兜底方法
+    public String deal_testHotKey (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
     }
 }
