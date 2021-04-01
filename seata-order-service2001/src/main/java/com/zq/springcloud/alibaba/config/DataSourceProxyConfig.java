34519bc69ab1f816e212cc3764cb019851d24eae
zqloner <zqloner@163.com>
2021-04-01 16:43:19 +0800

    - Seata事务没有搞得明白。改天再搞吧。

--- seata-order-service2001/src/main/java/com/zq/springcloud/alibaba/config/DataSourceProxyConfig.java
+++ seata-order-service2001/src/main/java/com/zq/springcloud/alibaba/config/DataSourceProxyConfig.java
@@ -25,43 +25,25 @@ import javax.sql.DataSource;
 public class DataSourceProxyConfig {
 
     @Value("${mybatis.mapperLocations}")
-
     private String mapperLocations;
 
-
     @Bean
-
     @ConfigurationProperties(prefix = "spring.datasource")
-
     public DataSource druidDataSource() {
-
         return new DruidDataSource();
-
     }
 
-
     @Bean
-
     public DataSourceProxy dataSourceProxy(DataSource dataSource) {
-
         return new DataSourceProxy(dataSource);
-
     }
 
-
     @Bean
-
     public SqlSessionFactory sqlSessionFactoryBean(DataSourceProxy dataSourceProxy) throws Exception {
-
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-
         sqlSessionFactoryBean.setDataSource(dataSourceProxy);
-
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
-
         sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
-
         return sqlSessionFactoryBean.getObject();
-
     }
 }
